b6f4c1ce42ba5da03632c1f43b24cb021ee6582e
Charles Painter <cpainter85@gmail.com>
2015-03-14 12:26:41 -0600

    crud aliens and human spec file

---
+++ spec/features/01_crud_aliens_and_people.rb
@@ -0,0 +1,146 @@
+require 'rails_helper'
+
+feature 'Users can CRUD aliens and humans' do
+
+  before :each do
+    @alien1 = Alien.create!(species: 'Martians', planet: 'Mars')
+    @alien2 = Alien.create!(species: 'Daemonites', planet: 'Daemon')
+    @human1 = Human.create!(first_name: 'Will', last_name: 'Smith', occuption: 'Actor')
+    @human2 = Human.create!(first_name: 'Bob', last_name: 'Bobson', occuption: 'Farmer')
+  end
+
+  scenario 'index lists all aliens and humans' do
+    visit root_path
+
+    expect(page).to have_content 'List of Aliens'
+    expect(page).to have_content 'Martians from the planet Mars'
+    expect(page).to have_content 'Daemonites from the planet Daemon'
+    expect(page).to have_content 'List of Humans'
+    expect(page).to have_content 'Will Smith - Actor'
+    expect(page).to have_content 'Bob Bobson - Farmer'
+  end
+
+  scenario 'User can add a new Alien' do
+    visit root_path
+    click_link 'New Alien'
+
+    expect(current_path).to eq new_alien_path
+    expect(page).to have_content 'Add New Alien'
+
+    fill_in 'Species', with: 'Kree'
+    fill_in 'Planet', with: 'Hala'
+    click_link 'Create Alien'
+
+    expect(current_path).to eq root_path
+    expect(page).to have_content 'Kree were successfully added!'
+    expect(page).to have_content 'Kree from the planet Hala'
+  end
+
+  scenario 'User can add a new Human' do
+    visit root_path
+    click_link 'New Human'
+
+    expect(current_path).to eq new_human_path
+    expect(page).to have_content 'Add New Human'
+
+    fill_in 'First Name', with: 'Dan'
+    fill_in 'Last Name', with: 'Dare'
+    fill_in 'Occupation', with: 'Hero'
+    click_link 'Create Human'
+
+    expect(current_path).to eq root_path
+    expect(page).to have_content 'Dan Dare was successfully added!'
+    expect(page).to have_content 'Dan Dare - Hero'
+  end
+
+  scenario 'index links to Alien show' do
+
+    visit root_path
+    click_link 'Martians from the planet Mars'
+
+    expect(current_path).to eq alien_path(@alien1)
+    expect(page).to have_content 'Species: Martians'
+    expect(page).to have_content 'Planet: Mars'
+    expect(find_link('Index'[:href]).to eq(root_path)
+    expect(find_link('Edit'[:href]).to eq(edit_alien_path(@alien1))
+    expect(find_link('Delete'[:href]).to eq(alien_path(@alien1))
+
+  end
+
+  scenario 'index links to Human show' do
+
+    visit root_path
+    click_link 'Will Smith - Actor'
+
+    expect(current_path).to eq human_path(@human1)
+    expect(page).to have_content 'First Name: Will'
+    expect(page).to have_content 'Last Name: Smith'
+    expect(page).to have_content 'Occupation: Actor'
+    expect(find_link('Index'[:href]).to eq(root_path)
+    expect(find_link('Edit'[:href]).to eq(edit_human_path(@human1))
+    expect(find_link('Delete'[:href]).to eq(human_path(@human1))
+
+  end
+
+  scenario 'User can update Alien' do
+    visit alien_path(@alien1)
+    click_link 'Edit'
+    expect(current_path).to eq edit_alien_path(@alien1)
+    expect(page).to have_content 'Edit Alien'
+
+    fill_in 'Species', with: 'Wookie'
+    fill_in 'Planet', with: 'Kashyyyk'
+    click_link 'Update Alien'
+
+    expect(current_path).to eq alien_path(@alien1)
+    expect(page).to have_content 'Alien was updated!'
+    expect(page).to have_content 'Species: Wookie'
+    expect(page).to have_content 'Planet: Kashyyk'
+    expect(page).to have_no_content 'Martians'
+    expect(page).to have_no_content 'Mars'
+  end
+
+  scenario 'User can update Human' do
+    visit human_path(@human1)
+    click_link 'Edit'
+    expect(current_path).to eq edit_human_path(@human1)
+    expect(page).to have_content 'Edit Human'
+
+    fill_in 'First Name', with: 'Peter'
+    fill_in 'Last Name', with: 'Parker'
+    fill_in 'Occupation', with: 'Spider-Man'
+    click_link 'Update Human'
+
+    expect(current_path).to eq human_path(@human1)
+    expect(page).to have_content 'Human was updated!'
+    expect(page).to have_content 'First Name: Peter'
+    expect(page).to have_content 'Last Name: Parker'
+    expect(page).to have_content 'Occupation: Spider-Man'
+    expect(page).to have_no_content 'Will'
+    expect(page).to have_no_content 'Smith'
+    expect(page).to have_no_content 'Actor'
+  end
+
+  scenario 'User can delete Alien' do
+    visit alien_path(@alien1)
+    click_link 'Delete'
+
+    expect(current_path).to eq root_path
+    expect(page).to have_content 'Martians were successfully destroyed'
+    expect(page).to have_no_content 'Martians from the planet Mars'
+
+    expect { @alien1.reload }.to raise_error ActiveRecord::RecordNotFound
+  end
+
+  scenario 'User can delete Human' do
+    visit human_path(@human1)
+    click_link 'Delete'
+
+    expect(current_path).to eq root_path
+    expect(page).to have_content 'Will Smith has been killed!'
+    expect(page).to have_no_content 'Will Smith - Actor'
+
+    expect { @human1.reload }.to raise_error ActiveRecord::RecordNotFound
+  end
+
+end
